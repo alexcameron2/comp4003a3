e8858adf2a6f709878780d82b072dda871fe54f5
alexcameron2 <onboardrope@gmail.com>
2015-10-18 23:02:06 -0400

    Commit

--- Part1.sql
+++ Part1.sql
@@ -3,7 +3,7 @@ DECLARE
 	
 	command2 varchar2(10000) := 'CREATE TABLE Course(c_ID varchar2(9) PRIMARY KEY, c_name varchar2(6) NOT NULL, c_loc varchar2(7), c_days varchar2(3), c_time varchar2(10), UNIQUE(c_name), CHECK (c_days IN (''MWF'', ''TR'')), CHECK (c_time IN (''8:35-9:25'', ''10:35-11:25'', ''1:35-2:25'', ''3:35-4:25'')))';	
 
-	command3 varchar2(10000) := 'CREATE TABLE Grade (s_ID integer, c_ID varchar2(9), integer mark, PRIMARY KEY (s_ID, c_ID))';
+	command3 varchar2(10000) := 'CREATE TABLE Grade (s_ID integer, c_ID varchar2(9),mark integer, PRIMARY KEY (s_ID, c_ID), FOREIGN KEY (s_ID) REFERENCES Student(s_ID), FOREIGN KEY (c_ID) REFERENCES Course(c_ID))';
 
 BEGIN
 	--execute immediate drop1;
